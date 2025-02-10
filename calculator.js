ccc7ed36bab787fdb28e124a23a48f7a5e20374c
Tanecodes <tanecodes@gmail.com>
2025-02-10 16:05:35 +1000

    fix: buttons numbers now show in display area and created a clear button to clear the screen

---
+++ calculator.js
@@ -0,0 +1,14 @@
+const numbers = document.querySelectorAll(".num");
+const equals = document.querySelector(".equals");
+const clear = document.querySelector(".clear");
+const display = document.querySelector(".display");
+  
+numbers.forEach(function(button) {
+  button.addEventListener("click", function() {
+    display.textContent += button.textContent;
+  }); 
+});
+
+clear.addEventListener("click",function(){
+  display.textContent = "";
+});
--- index.js
+++
@@ -1,6 +0,0 @@
-const display = document.getElementById("#display");
-const btn = document.querySelector("button");
-
-btn.click(function(){
-  display
-});
