bf8414e6b97da5ec29e7e9431cecf20a5f71d131
Tanecodes <tanecodes@gmail.com>
2025-02-12 02:09:02 +1000

    my first javascript project its basic and crappy but im proud of it baby steps still strugling to understand the equals js code though

--- calculator.js
+++ calculator.js
@@ -1,33 +1,41 @@
 const numbers = document.querySelectorAll(".num");
 const display = document.querySelector(".display");
 
-let previnput = "";
-let currentinput = "";
-let operator = "";
-let result = "";
-  
+// number buttons loop  
 numbers.forEach(function(button) {
   button.addEventListener("click", function() {
     display.textContent += button.textContent;
   }); 
 });
+// clear button
 document.querySelector(".clear").addEventListener("click",function(){
   display.textContent = "";
 });
+// add button
 document.querySelector(".add").addEventListener("click",function(){
   let add = document.querySelector(".add").textContent;
   display.textContent = display.textContent + add;
 });
+// minus button
 document.querySelector(".minus").addEventListener("click",function(){
   let minus = document.querySelector(".minus").textContent;
   display.textContent = display.textContent + minus;
 });
+// times button
 document.querySelector(".times").addEventListener("click",function(){
   let times = document.querySelector(".times").textContent;
   display.textContent = display.textContent + times;
 })
-document.querySelector("equals").addEventListener("click",function(){
+//equals button
+document.querySelector(".equals").addEventListener("click",function(){
   let expression = display.textContent;
-  
+  expression = expression.replace("x","*");
+
+  try{
+    let result = eval(expression);
+    display.textContent = result;
+  }catch{(error)
+    display.textContent = "error"
+  }
 });
  
